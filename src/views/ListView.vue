8e06dd710e8cbdd31f1febb9845a4cfa28b3ea1e
jkson83 <ugwha@naver.com>
2024-07-26 17:24:57 +0900

    20240726

--- src/views/ListView.vue
+++ src/views/ListView.vue
@@ -1,24 +1,33 @@
 <template>
-    <h1>Hello, {{ title }}</h1>
+    <h1>Table {{ title }}</h1>
     <div class="listTable">
-      <ul>
+      <ul class="row-group">
         <li>번호</li>
         <li>제목</li>
         <li>ID</li>
         <li>날짜</li>
         <li>조회</li>
       </ul>
+      <ul>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+      </ul>
     </div>
+
     <write-form></write-form>
 </template>
 
 <script>
 import WriteForm from './WriteForm.vue'
+
 export default {
   components: { WriteForm },
+
   data () {
     return {
-      title: 'World',
+      title: 'Study',
       name: 'WriteForm',
       components: {
         WriteForm
