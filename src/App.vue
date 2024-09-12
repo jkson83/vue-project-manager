94212dddfe1d58a167c9b40eb24a46311d9403e0
손진경 <jk.son83@netive.co.kr>
2024-09-12 16:29:27 +0900

    uptest

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,9 @@
 <template>
   <nav>
     <router-link to="/">Home</router-link>
-    <router-link to="/about">About</router-link>
-    <router-link to="/list">List</router-link>
-    <router-link to="/Nested">Nested</router-link>
+    <router-link to="/about">- About</router-link>
+    <router-link to="/list">- List</router-link>
+    <router-link to="/Nested">- Nested</router-link>
   </nav>
   <div class="article">
   <router-view />
