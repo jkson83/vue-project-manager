a856b14d4b782e87833a326dd378dfa1ea54048b
손진경 <jk.son83@netive.co.kr>
2024-12-06 17:23:02 +0900

    [241206]_프로젝트 재정리

--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
-import './assets/main.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
+//import 'bootstrap/dist/css/bootstrap.min.css';
+//import 'bootstrap/dist/js/bootstrap.js';
 import App from './App.vue';
 import router from './router';
-
+import './assets/main.css';
 import { createApp } from 'vue';
 import { createPinia } from 'pinia';
 
@@ -13,4 +13,3 @@ app.use(router);
 app.use(pinia);
 
 app.mount('#app'); //인스턴스를 실행할 컨텐이너를 설정
-import 'bootstrap/dist/js/bootstrap.js';
